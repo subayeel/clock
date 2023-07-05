f3bad009257efdc083601186d4087e1ed40a4d0e
vishal <vishaliscoding27@gmail.com>
2023-07-05 14:38:55 +0530

    merged with main

--- src/App.js
+++ src/App.js
@@ -1,31 +1,25 @@
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import Subayeel from "./Components/Subayeel";
-<<<<<<< HEAD
 import Alarm from "./Components/Alarm";
-=======
 import Timer from "./Components/Timer";
 import Stopwatch from "./Components/stopwatch";
 
 import Layout from "./Components/Layout";
->>>>>>> 785c5db5cb2ac0de23f611b858f482dd9f474be8
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-<<<<<<< HEAD
-          <Route path="/subayeel" element={<Subayeel />}></Route>
-          <Route path="/alarm" element={<Alarm />}></Route>
-=======
+
           {/* Add your Components here */}
 
           <Route path="/" element={<Layout />}>
             <Route path="/Stopwatch" element={<Stopwatch />}></Route>
-            <Route path="/subayeel" element={<Subayeel />}></Route>
+
             <Route path="/timer" element={<Timer />}></Route>
+            <Route path="/alarm" element={<Alarm />}></Route>
           </Route>
->>>>>>> 785c5db5cb2ac0de23f611b858f482dd9f474be8
         </Routes>
       </Router>
     </div>
