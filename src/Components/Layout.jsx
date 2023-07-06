3fc101a6f806a93772811625e293c5eada3828e2
subayeel <abdullahsubayeel999@gmail.com>
2023-07-06 11:09:13 +0530

    updated side bar

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,8 +1,46 @@
 import React, { useState } from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation, useNavigate } from "react-router-dom";
 import clock from "../Assets/Images/clock.png";
+import { Alarm, AvTimer, DarkMode, Language, Timer } from "@mui/icons-material";
+import styled from "styled-components";
+
+const StyledAlarm = styled(Alarm)`
+  color: #eab2a0;
+  height: 50px;
+  width: 50px;
+  
+`;
+const StyledTimer = styled(Timer)`
+  color: #eab2a0;
+  height: 50px;
+  width: 50px;
+  
+`;
+const StyledStopWatch = styled(AvTimer)`
+  color: #eab2a0;
+  height: 50px;
+  width: 50px;
+  
+`;
+const StyledWorldClock = styled(Language)`
+  color: #eab2a0;
+  height: 50px;
+  width: 50px;
+  
+`;
+const StyledDarkMode = styled(DarkMode)`
+  color: white;
+  height: 36px;
+  width: 36px;
+  &:hover{
+    color: grey;
+    cursor: pointer;
+  }
+`;
 function Layout() {
   const [isDark, setDark] = useState(localStorage.getItem("dark") || false);
+  const location = useLocation();
+  const navigate= useNavigate()
   return (
     <>
       <nav>
@@ -13,20 +51,56 @@ function Layout() {
           </span>
           <ul>
             <li>
-              <Link to="/time">Time</Link>
+              <Link to="/holidays">Holidays</Link>
             </li>
-            <li>
-              <Link to="/stopwatch">Stop Watch</Link>
-            </li>
-            <li>
-              <Link to="/timer">Timer</Link>
-            </li>
-            <li>
-              <Link to="/alarm">Alarmclock</Link>
-            </li>
+            <li><StyledDarkMode/></li>
           </ul>
         </header>
       </nav>
+      <aside>
+        <ul>
+          <li
+            style={
+              location.pathname.includes("/alarm") ? { background: "grey" } : {}
+            }
+            onClick={()=>navigate("/alarm")}
+          >
+            <StyledAlarm></StyledAlarm>
+            <p>Alarm Clock</p>
+          </li>
+          <li
+            style={
+              location.pathname.includes("/timer") ? { background: "grey" } : {}
+            }
+            onClick={()=>navigate("/timer")}
+          >
+            <StyledTimer />
+            <p>Timer</p>
+          </li>
+          <li
+            style={
+              location.pathname.includes("/stopwatch")
+                ? { background: "grey" }
+                : {}
+            }
+            onClick={()=>navigate("/alarm")}
+          >
+            <StyledStopWatch />
+            <p>Stopwatch</p>
+          </li>
+          <li
+            style={
+              location.pathname.split("/").includes("time")
+                ? { background: "grey" }
+                : {}
+            }
+            onClick={()=>navigate("/time")}
+          >
+            <StyledWorldClock />
+            <p>World Clock</p>
+          </li>
+        </ul>
+      </aside>
       <Outlet />
     </>
   );
